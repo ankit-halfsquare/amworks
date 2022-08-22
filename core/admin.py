3a2b35cf9585367db9cb29324a4cc3470daf45ea
ankit <ankit.kalal.halfsquare@gmail.com>
2022-08-22 15:31:00 +0530

    first commit

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Building, Project
+
+admin.site.register(Building)
+admin.site.register(Project)
---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,HttpResponse
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("hello")
