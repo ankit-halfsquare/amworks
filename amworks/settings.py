98181ce0d075019019d759ca04195fade395e415
ankit <ankit.kalal.halfsquare@gmail.com>
2022-09-06 16:54:15 +0530

    url change

--- amworks/settings.py
+++ amworks/settings.py
@@ -140,17 +140,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-# STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
-# STATIC_URL = STATIC_HOST + "/static/"
-
-
-# STATIC_URL = '/static/'
-# # STATICFILES_DIRS =[
-# #     os.path.join(BASE_DIR, 'static')
-# # ]
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 STATIC_URL = STATIC_HOST + "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
